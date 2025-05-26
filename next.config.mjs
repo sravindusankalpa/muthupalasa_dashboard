8049ede9aad7607810634bae004de35e3341dc02
Ravindu Sankalpa <sravindusankalpa@gmail.com>
2025-05-26 19:38:38 +0530

    fix prod issue

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer'],
+  },
+};
 
-export default nextConfig
+module.exports = nextConfig;
