e110051ddc4c16c1f1d99e7d880e95342c3f0843
sravindusankalpa <sravindusankalpa@gmail.com>
2025-06-17 17:46:19 +0530

    update collection references from MP7 to MP8 in CRUD interfaces

--- components/crud-interface-simple.tsx
+++ components/crud-interface-simple.tsx
@@ -184,7 +184,7 @@ export function CrudInterface({ database, collection, title, description }: Crud
         nic: getDisplayValue(doc, "dealerInfo.ownerNIC"),
         shop: getDisplayValue(doc, "dealerInfo.shopName"),
       }
-    } else if (collection.includes("EventDaySubmission") || collection.includes("MP7")) {
+    } else if (collection.includes("EventDaySubmission") || collection.includes("MP8")) {
       return {
         name: getDisplayValue(doc, "eventuserdata.ownerName"),
         nic: getDisplayValue(doc, "eventuserdata.ownerNIC"),
